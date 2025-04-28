8f5ba88371960f91bdbe81624996c493490d6f64
Faister Cabrera Carvalho <faister.carvalho@toptal.com>
2025-04-28 11:25:09 -0300

    feat: default flex-direction to row on native to match behavior on web

--- src/flexbox-and-grid/flex.ts
+++ src/flexbox-and-grid/flex.ts
@@ -1,13 +1,19 @@
 import { css } from "css-native";
+import { Platform } from "react-native";
 
 export const flex = (value?: TemplateStringsArray) =>
   (value?.[0] &&
     css`
       flex: ${value[0].includes("/") ? `calc(${value[0]} * 100%)` : value[0]};
     `) ||
-  css`
-    display: flex;
-  `;
+  Platform.OS === "web"
+    ? css`
+        display: flex;
+      `
+    : css`
+        display: flex;
+        flex-direction: row;
+      `;
 export const flex_auto = flex`1 1 auto`;
 export const flex_initial = flex`0 1 auto`;
 export const flex_none = flex`none`;
