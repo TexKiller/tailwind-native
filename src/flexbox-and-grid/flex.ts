f96df916ea44e0e0175af9137dff052253efdb61
Faister Cabrera Carvalho <faister.carvalho@toptal.com>
2025-04-29 01:45:41 -0300

    fix: revert flex-direction: row for native

--- src/flexbox-and-grid/flex.ts
+++ src/flexbox-and-grid/flex.ts
@@ -1,19 +1,13 @@
 import { css } from "css-native";
-import { Platform } from "react-native";
 
 export const flex = (value?: TemplateStringsArray) =>
   (value?.[0] &&
     css`
       flex: ${value[0].includes("/") ? `calc(${value[0]} * 100%)` : value[0]};
     `) ||
-  Platform.OS === "web"
-    ? css`
-        display: flex;
-      `
-    : css`
-        display: flex;
-        flex-direction: row;
-      `;
+  css`
+    display: flex;
+  `;
 export const flex_auto = flex`1 1 auto`;
 export const flex_initial = flex`0 1 auto`;
 export const flex_none = flex`none`;
